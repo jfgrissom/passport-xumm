15a4b7adb044968aa791da2fd852af67fb57bb43
jfgrissom <jfgrissom@gmail.com>
2021-10-15 16:59:16 -0700

    Extending Passport Strategy.

---
+++ lib/passport-xumm/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Module dependencies.
+ */
+import { Strategy } from './strategy'
+
+/**
+ * Pull package.json into the distributable package.
+ */
+import { name, version } from '../../package.json'
+
+/**
+ * Expose constructors.
+ */
+export { Strategy, name, version }
--- lib/passport-xumm/strategy.ts
+++ lib/passport-xumm/strategy.ts
@@ -1,3 +1,8 @@
+import { Strategy as PassportStrategy } from 'passport-strategy'
+import util from 'util'
+
+/* 
+TODO: These env vars need to be passed to the strategy when it's initialized.
 import { XummSdk, XummTypes } from 'xumm-sdk'
 import * as dotenv from 'dotenv'
 
@@ -25,5 +30,32 @@ const main = async () => {
   const response = await sdk.payload.create(request)
   console.log(response)
 }
+*/
 
-main()
+interface iXummStrategyProps {
+  pubKey: string
+  pvtKey: string
+}
+
+export class XummStrategy extends PassportStrategy {
+  constructor(props: iXummStrategyProps) {
+    super()
+    const { pubKey, pvtKey } = props
+
+    this.pubKey = pubKey
+    this.pvtKey = pvtKey
+    this.name = 'xumm'
+
+  }
+
+  public pubKey: string
+  private pvtKey: string
+  public name: string
+
+  // 1. Get a QR code for the view.
+  // 2. 
+
+  // Implement a superclass method override.
+  authenticate(req: {}, options: {}) { }
+
+}
