c7e7b20f2f9fba948515aafceb744a373cfe1b6f
jfgrissom <jfgrissom@gmail.com>
2021-10-16 14:24:19 -0700

    Implemented function that returns a QR code from Xumm Service.

--- lib/passport-xumm/strategy.ts
+++ lib/passport-xumm/strategy.ts
@@ -1,61 +1,54 @@
 import { Strategy as PassportStrategy } from 'passport-strategy'
-import util from 'util'
 
-/* 
-TODO: These env vars need to be passed to the strategy when it's initialized.
+//TODO: These env vars need to be passed to the strategy when it's initialized.
 import { XummSdk, XummTypes } from 'xumm-sdk'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const pubKey = process.env.XUMM_PUB_KEY
-const pvtKey = process.env.XUMM_PVT_KEY
-const sdk = new XummSdk(pubKey, pvtKey)
-
-if (!pubKey) throw Error("Your public Xumm key can't be found in .env XUMM_PUB_KEY")
-if (!pvtKey) throw Error("Your private Xumm key can't be found in .env XUMM_PVT_KEY")
-if (!sdk) throw Error("There was a problem initializing Xumm.")
-
-const main = async () => {
-  const request: XummTypes.XummPostPayloadBodyJson = {
-    options: {
-      submit: false,
-      expire: 240
-    },
-    txjson: {
-      TransactionType: "SignIn"
-    }
-  }
-
-  const response = await sdk.payload.create(request)
-  console.log(response)
-}
-*/
-
 interface iXummStrategyProps {
   pubKey: string
   pvtKey: string
+  timeout?: number
 }
 
 export class XummStrategy extends PassportStrategy {
   constructor(props: iXummStrategyProps) {
     super()
-    const { pubKey, pvtKey } = props
+    const { pubKey, pvtKey, timeout } = props
 
+    this.timeout = timeout || 240
     this.pubKey = pubKey
-    this.pvtKey = pvtKey
-    this.name = 'xumm'
+    this.name = 'Xumm Strategy For PassportJS'
+    this.sdk = new XummSdk(pubKey, pvtKey)
+
+    if (!pubKey) throw Error('Xumm public API key is required.')
+    if (!pvtKey) throw Error('Xumm private API secret is required.')
+    if (!this.sdk) throw Error('There was a problem initializing Xumm.')
   }
 
+  public timeout: number
   public pubKey: string
-  private pvtKey: string
   public name: string
-
-  // Where this fits into the entire picture.
-  // This function is called once the QR code is read and a 
-  // response has been sent to the backend.
+  private sdk: XummSdk
 
   // Implement a superclass method override.
-  authenticate(req: {}, options: {}) { }
+  // This runs a data base check that the consumer passes in.
+  authenticate = (req: {}, options: {}) => {}
 
+  presetQrCode = async () => {
+    const { sdk } = this
+    const request: XummTypes.XummPostPayloadBodyJson = {
+      options: {
+        submit: false,
+        expire: 240
+      },
+      txjson: {
+        TransactionType: 'SignIn'
+      }
+    }
+
+    const response = await sdk.payload.create(request)
+    return response
+  }
 }
